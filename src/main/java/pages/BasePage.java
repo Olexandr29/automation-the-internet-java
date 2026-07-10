6acdba2ea99958ae7e381ead03e21e0845ffbd94
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-07-10 17:56:11 +0300

    feat: integrate Allure locally

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -17,7 +17,7 @@ public abstract class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
     protected WebElement find(By locator) {
