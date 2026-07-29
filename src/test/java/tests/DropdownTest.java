525a701e486a8afda2fe7f42b953948dc2dadef6
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-07-29 08:35:23 +0300

    feat: organize test suites by groups and classes

--- src/test/java/tests/DropdownTest.java
+++ src/test/java/tests/DropdownTest.java
@@ -5,30 +5,27 @@ import org.testng.annotations.Test;
 import pages.DropdownPage;
 import testData.DropdownData;
 
-import java.lang.reflect.Method;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+@Test(groups = {"regression"})
 public class DropdownTest extends BaseTest {
 
     private DropdownPage dropdownPage;
 
-    @Override
-    @BeforeMethod
-    public void setUp(Method method, Object[] testData) {
-        super.setUp(method, testData);
+    @BeforeMethod(alwaysRun = true, dependsOnMethods = "setUp")
+    public void openDropdown() {
         dropdownPage = homePage.openDropdownPage();
     }
 
-    @Test
+    @Test(groups = {"smoke"})
     public void TC21DropdownDefaultState() {
         assertEquals(driver.getCurrentUrl(), DropdownData.URL_DROPDOWN_PAGE, "The Dropdown page should be opened.");
         assertTrue(dropdownPage.isDropdownVisible(), "The dropdown should be visible.");
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_DEFAULT, String.format("The default selected value should be %s", DropdownData.OPTION_DEFAULT));
     }
 
-    @Test
+    @Test(groups = {"smoke"})
     public void TC22VerifyDropdownOptionsDisplayed() {
         assertEquals(dropdownPage.amountOfAvailableDropdownOptions(), DropdownData.AMOUNT_OF_DROPDOWN_OPTIONS, "The dropdown should contain %d items and unexpected options should not be displayed.".formatted(DropdownData.AMOUNT_OF_DROPDOWN_OPTIONS));
         assertTrue(dropdownPage.isSpecificOptionDisplayed(DropdownData.OPTION_DEFAULT), String.format("The '%s' should be displayed", DropdownData.OPTION_DEFAULT) );
@@ -36,13 +33,13 @@ public class DropdownTest extends BaseTest {
         assertTrue(dropdownPage.isSpecificOptionDisplayed(DropdownData.OPTION_2), String.format("The '%s' should be displayed", DropdownData.OPTION_2));
     }
 
-    @Test
+    @Test(groups = {"smoke"})
     public void TC23SelectOption1() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_1);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should became the selected value.", DropdownData.OPTION_1) );
     }
 
-    @Test
+    @Test(groups = {"smoke"})
     public void TC24VerifyOptionRemainsSelectedAfterReopeningAndClosing() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_2);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_2, String.format("The %s should became the selected value.", DropdownData.OPTION_2) );
@@ -52,7 +49,7 @@ public class DropdownTest extends BaseTest {
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_2, String.format("The %s should became the selected value.", DropdownData.OPTION_2) );
 }
 
-    @Test
+    @Test(groups = {"keyboard"})
     public void TC25ChangeSelectedOptionUsingKeyboardArrowKeys() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_1);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should became the selected value.", DropdownData.OPTION_1) );
@@ -62,7 +59,7 @@ public class DropdownTest extends BaseTest {
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should became the selected value.", DropdownData.OPTION_1) );
     }
 
-    @Test
+    @Test(groups = {"keyboard"})
     public void TC26NavigateSelectAndChangeDropdownOptionViaKeyboard() {
         dropdownPage.makeDropdownActive();
         assertTrue(dropdownPage.isDropdownActive(), "The Dropdown should be focused");
@@ -80,7 +77,7 @@ public class DropdownTest extends BaseTest {
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should became the selected value.", DropdownData.OPTION_1) );
     }
 
-    @Test
+    @Test(groups = {"keyboard"})
     public void TC27VerifyTheArrowUpAndDownOnTheFirstAndLastOptions() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_1);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should became the selected value.", DropdownData.OPTION_1) );
@@ -92,7 +89,7 @@ public class DropdownTest extends BaseTest {
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_2, String.format("The selected value should not be changed and the %s should remains selected.", DropdownData.OPTION_2) );
     }
 
-    @Test
+    @Test(groups = {"smoke"})
     public void TC28VerifyOnlyOneOptionCanBeSelectedAtaTime() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_1);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should became the selected value.", DropdownData.OPTION_1) );
@@ -100,7 +97,7 @@ public class DropdownTest extends BaseTest {
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_2, String.format("Only one option should be selected at a time, and now it should be the %s", DropdownData.OPTION_2) );
     }
 
-    @Test
+    @Test(groups = {"navigation"})
     public void TC29VerifySelectedOptionAfterRefresh() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_1);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_1, String.format("The %s should be selected", DropdownData.OPTION_1));
@@ -108,7 +105,7 @@ public class DropdownTest extends BaseTest {
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_DEFAULT, String.format("The default value %s should be selected", DropdownData.OPTION_DEFAULT));
     }
 
-    @Test
+    @Test(groups = {"navigation"})
     public void TC30VerifyBrowserBackAndForwardNavigationBehaviour() {
         dropdownPage.selectSpecificOption(DropdownData.OPTION_2);
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_2, String.format("The %s should be selected", DropdownData.OPTION_2));
@@ -119,23 +116,6 @@ public class DropdownTest extends BaseTest {
         assertEquals(driver.getCurrentUrl(), DropdownData.URL_DROPDOWN_PAGE, "The Dropdown page should be opened");
         assertEquals(dropdownPage.getSelectedDropdownText(), DropdownData.OPTION_2, String.format("- The %s should be displayed as selected", DropdownData.OPTION_2));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
