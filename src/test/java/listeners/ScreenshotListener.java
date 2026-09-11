e5373c5a8f4343f5d4cdeec6b0fb92706f2ae34f
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-09-11 17:59:16 +0300

    feat: replace onTestFailure method with afterInvocation one

--- src/test/java/listeners/ScreenshotListener.java
+++ src/test/java/listeners/ScreenshotListener.java
@@ -4,21 +4,36 @@ import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestListener;
+import org.slf4j.LoggerFactory;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
 import tests.BaseTest;
+import org.slf4j.Logger;
 
-public class ScreenshotListener implements ITestListener {
+
+public class ScreenshotListener implements IInvokedMethodListener {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
-    public void onTestFailure(ITestResult result) {
-        System.out.println(">>> ScreenshotListener.onTestFailure()");
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (!method.isTestMethod() ) {
+            return;
+        }
+        if (testResult.getStatus() != ITestResult.FAILURE) {
+            return;
+        }
+        logger.debug(">>> ScreenshotListener.afterInvocation()");
+        logger.debug(">>> Thread: " + Thread.currentThread().getName());
 
-        BaseTest test = (BaseTest) result.getInstance();
+        BaseTest test = (BaseTest) testResult.getInstance();
         WebDriver driver = test.getDriver();
 
         byte[] screenshot = ((TakesScreenshot) driver)
                 .getScreenshotAs(OutputType.BYTES);
+
+        logger.debug(">>> Screenshot captured: " + screenshot.length + " bytes");
 
         Allure.addAttachment(
                 "Screenshot on failure",
@@ -26,5 +41,6 @@ public class ScreenshotListener implements ITestListener {
                 new java.io.ByteArrayInputStream(screenshot),
                 ".png"
         );
+        logger.debug(">>> Screenshot attached");
     }
 }
