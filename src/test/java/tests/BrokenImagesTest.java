2f1bb8f0435057589b9e315a60f6bb2ec652ae4b
Olexandr Konoval <name.olexandr.konoval@gmail.com>
2026-09-21 10:27:30 +0300

    feat: manage test data

--- src/test/java/tests/BrokenImagesTest.java
+++ src/test/java/tests/BrokenImagesTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.BrokenImagesPage;
+import testData.BrokenImagesData;
 
 import static org.testng.Assert.*;
 import org.testng.asserts.SoftAssert;
@@ -17,23 +18,23 @@ public class BrokenImagesTest extends BaseTest {
 
     @Test
     public void TC36VerifyPageContent() {
-        assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/broken_images", "The Broken Images page URL is wrong");
+        assertEquals(driver.getCurrentUrl(), BrokenImagesData.BROKEN_IMAGES_URL, "The Broken Images page URL is wrong");
         assertTrue(brokenImagesPage.isBrokenImagesPageLoaded(), "The \"Broken Images\" page is not loaded");
-        assertTrue(brokenImagesPage.isHeaderVisible(), "The \"Broken Images\" header is not displayed");
-        assertEquals(brokenImagesPage.getImagesAmount(), 3, "the amount of Images are not 3");
+        assertTrue(brokenImagesPage.isHeaderVisible(), String.format("The '%s' header is not displayed", BrokenImagesData.HEADER));
+        assertEquals(brokenImagesPage.getImagesAmount(), BrokenImagesData.IMAGES_AMOUNT, String.format("the amount of Images are not %d", BrokenImagesData.IMAGES_AMOUNT));
         assertTrue(brokenImagesPage.isImageVisible(1), String.format("The Image %d is not displayed on the page", 1));
         assertTrue(brokenImagesPage.isImageVisible(2), String.format("The Image %d is not displayed on the page", 2));
         assertTrue(brokenImagesPage.isImageVisible(3), String.format("The Image %d is not displayed on the page", 3));
-        assertTrue(brokenImagesPage.isFooterVisible(), "The footer text \"Powered by Elemental Selenium\" is not displayed");
-        assertTrue(brokenImagesPage.isLinkVisible(), "The \"Elemental Selenium\" link is not displayed");
+        assertTrue(brokenImagesPage.isFooterVisible(), String.format("The footer text '%s' is not displayed", BrokenImagesData.FOOTER));
+        assertTrue(brokenImagesPage.isLinkVisible(), String.format("The '%s' link is not displayed", BrokenImagesData.LINK));
     }
 
     @Test
     public void TC37VerifyImageLoading() {
         SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(brokenImagesPage.isImageLoaded(3), "The third image is broken or not loaded.");
         softAssert.assertTrue(brokenImagesPage.isImageLoaded(1), "The first image is broken or not loaded.");
         softAssert.assertTrue(brokenImagesPage.isImageLoaded(2), "The second image is broken or not loaded.");
-        softAssert.assertTrue(brokenImagesPage.isImageLoaded(3), "The third image is broken or not loaded.");
         softAssert.assertAll();
     }
 
